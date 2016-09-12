53bbfd6f542035e17f1ed137f61785f7f479a796
Blake Scala and Tim Jung <blake@>
2016-09-12 16:19:21 -0700

    create front end with spark

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,34 @@
 import java.io.Console;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
 import java.util.List;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args){
-    Console myConsole = System.console();
-    System.out.println("Enter a number:");
-    String stringUserNumber = myConsole.readLine();
-    int intUserNumber = Integer.parseInt(stringUserNumber);
-    PingPong pingPong = new PingPong();
-    List<Object> pingPongResult = pingPong.runPingPong(intUserNumber);
-    System.out.println(pingPongResult);
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/output", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/output.vtl");
+      String input = request.queryParams("input-number");
+      Integer inputInteger = Integer.parseInt(input);
+      model.put("number-input", inputInteger);
+      PingPong pingPong = new PingPong();
+      List<Object> result = pingPong.runPingPong(inputInteger);
+      model.put("result", result);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
